0e6e8461622f9be24a19698cb1eb92a562ed2589
ParkBumSuo <qjatn1102@naver.com>
2025-01-13 17:58:52 +0900

    01_13완료분

--- src/main/java/com/example/demo/persistance/ReservationRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.persistance;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.domain.Reservation;
-
-public interface ReservationRepository extends JpaRepository<Reservation, Long>{
-
-}
